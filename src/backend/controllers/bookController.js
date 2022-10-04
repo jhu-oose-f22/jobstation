3e5de84c1f64b9bc42b5502e72d0caab7b67dfda
qtniu <qitong.niu@gmail.com>
2022-10-04 13:15:59 -0400

    initial commit

---
+++ src/backend/controllers/bookController.js
@@ -0,0 +1,122 @@
+const { findConfigUpwards } = require('@babel/core/lib/config/files')
+const Book = require('../models/Book')
+const { book } = require('../router')
+exports.viewCreateScreen = function(req, res) {
+    res.render('add-book')
+}
+     
+exports.viewSearchScreen = function(req, res) { 
+    res.render('search-book')
+    // console.log(req.body)  
+} 
+      
+exports.create = function(req, res) {
+    // console.log(req.body)
+    let book = new Book(req.body, req.session.user._id)
+    book.create().then(function(newId) { 
+        // the return value of book.create() is what it resolve(), 
+        // and the return value can be used directly as
+        // the argument of the then() function(e.g., newId, here)
+        // res.send("new book created")
+        req.flash("success", "new book created")
+        req.session.save(() => res.redirect(`/book/${newId}`))
+    }).catch(function(errors) {
+        // res.send(errors)
+        errors.forEach(error => req.flash("errors", error))
+        req.session.save(() => res.redirect("/add-book"))
+    })
+}
+
+exports.viewSingle = async function(req, res) {
+
+    try {
+        let book = await Book.findSingleById(req.params.id, req.visitorId)
+        res.render('single-book-screen', {book: book})
+    } catch {
+        // res.send("404 not found")
+        res.render('404')
+    }
+}
+
+exports.viewEditScreen = async function(req, res) {
+    try {
+        let book = await Book.findSingleById(req.params.id, req.visitorId)
+        // { sample of returned book
+        //     _id: new ObjectId("6317fced299500f0846bcf28"),
+        //     title: 'another book',
+        //     body: 'asdfasfd asdf asf \r\nasdfasf\r\nsadfzcvvvvzcvzcvzxv\\\r\n324234234',
+        //     createdDate: 2022-09-07T02:07:41.661Z,
+        //     authorId: undefined,
+        //     author: {
+        //       username: 'user7',
+        //       avatar: 'https://gravatar.com/avatar/9a1c72396097ca4a7985f4dbf56b79ed?s=128'
+        //     },
+        //     isVisitorOwner: true
+        //   }
+        if (book.isVisitorOwner) {
+            res.render("edit-book", {book: book})
+        } else {
+            req.flash("errors", "you have no permission to the requested page")
+            req.session.save(() => res.redirect("/"))
+        }
+    } catch {
+        res.render('404')
+    }
+}
+
+exports.edit = function(req, res) {
+    let book = new Book(req.body, req.visitorId, req.params.id)
+    book.update().then((status) => {
+        // the book was updated in the database
+        // or user did have permission, but there were validation errors
+        if (status == "success") {
+            // book was updated
+            req.flash("success", "updated.")
+            req.session.save(function() {
+                res.redirect(`/book/${req.params.id}/edit`)
+            })
+        } else {
+            // 
+            book.errors.forEach(function(error) {
+                req.flash("errors", error)
+            })
+            req.session.save(function() {
+                res.redirect(`/book/${req.params.id}/edit`)
+            })
+        }
+    }).catch(() => {
+        // a book with the requested id doesn't exist
+        // or the current visitor is not the owner of the book
+        req.flash("errors", "you have no permission to modify it")
+        req.session.save(function() {
+            res.redirect("/")
+        })
+    })
+}
+
+exports.search = function(req, res) {
+    // console.log(" search by title: " + req.body.title)
+    Book.search(req.body.related).then(books => {
+        console.log(req.body)
+        // res.render('search-result')
+        // console.log(books)
+        res.render('search-result', {
+            books: books,
+            profileUsername: req.session.user.username,
+            profileAvatar: req.session.user.avatar
+        })
+    }).catch(() => {
+        res.render('404')
+    })
+}
+
+exports.delete = function(req, res) {
+    Book.delete(req.params.id, req.visitorId).then(() => {
+        req.flash("success", "successfully deleted")
+        req.session.save(() => {res.redirect(`/profile/${req.session.user.username}`)})
+    }).catch(() => {
+        req.flash("errors", "you have no permission to delete it")
+        req.session.save(() => res.redirect("/"))
+    })
+}
+
---
+++ src/backend/controllers/userController.js
@@ -0,0 +1,131 @@
+const User = require('../models/User')
+const Book = require('../models/Book')
+const { book } = require('../router')
+const ObjectID = require('mongodb').ObjectID
+
+exports.mustBeLoggedIn = function(req, res, next) {
+    if (req.session.user) {
+        next()
+    } else {
+        req.flash("errors", "you must be logged in to perform that action")
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }
+}
+
+exports.login = function(req, res) {
+    let user = new User(req.body)
+    user.login().then(function(result) {
+        req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
+        // res.send(result)
+        req.session.save(function() { // callback function
+            res.redirect('/')
+        })
+    }).catch(function(err) {
+        // res.send(err)
+        req.flash('errors', err)
+        req.session.save(function() {
+            res.redirect('/')
+        })
+
+    })
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/')
+    })
+    // res.send("logged out")
+    
+}
+
+exports.register = function(req, res) {
+    // console.log(req.body)
+    user = new User(req.body)
+    user.register().then(() => {
+        req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }).catch((regErrors) => {
+        // res.send(user.errors)
+        regErrors.forEach(function(error) {
+            req.flash('regErrors', error)
+        })
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    })
+    
+}
+
+
+exports.home = function(req, res) {
+
+    if (req.session.user) {
+        // res.render('home-dashboard')
+        // console.log(req.params.username)
+        // console.log(req.session)
+        Book.findByOwnerId(req.profileUser._id).then(function(books) {
+            res.render('lib-dashboard', {
+                books: books,
+                profileUsername: req.profileUser.username,
+                profileAvatar: req.profileUser.avatar
+            })
+            // res.render('home-dashboard')
+        }).catch(function() {
+            // console.log("posts not found")
+            res.render('404')
+        })
+        
+    } else {
+        res.render('home-guest', {regErrors: req.flash('regErrors')})
+    }
+}
+
+exports.userHome = function(req, res, next) {
+    if (req.session.user) {
+        User.findByUsername(req.session.user.username).then(function(userDocument) {
+            req.profileUser = userDocument
+            // console.log("user found")
+            next()
+        }).catch(function() {
+            // console.log("no such user")
+            res.render("404")
+        })
+    }
+    else {
+        next()
+    }
+    
+}
+
+exports.ifUserExists = function(req, res, next) {
+    // console.log(req.params.username)
+    // console.log(req.session)
+    User.findByUsername(req.params.username).then(function(userDocument) {
+        req.profileUser = userDocument
+        // console.log("user found")
+        next()
+    }).catch(function() {
+        // console.log("no such user")
+        res.render("404")
+    })
+}
+
+exports.profileBooksScreen = function(req, res) {
+    // ask our post model for posts by a certain user by id
+    Book.findByOwnerId(req.profileUser._id).then(function(books) {
+        // console.log("profile id: " + req.profileUser._id)
+        console.log("profile books screen")
+        console.log(books)
+        res.render('profile', {
+            books: books,
+            profileUsername: req.profileUser.username,
+            profileAvatar: req.profileUser.avatar
+        })
+    }).catch(function() {
+        res.render('404')
+    })
+}
