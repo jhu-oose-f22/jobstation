a66a61ecc2b2950f798aa627050bf1f0ffbf604a
Boyuan Wang <davidwang.wby@outlook.com>
2022-10-16 12:26:21 -0400

    Rearrange Folder, add more logos

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, applyMiddleware, compose } from 'redux';
-
-import reducer
 
 import App from './App.js';
 
