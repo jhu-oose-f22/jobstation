a5e69da90b61fd3a9ea99909b9c2404197dd7f75
Zhen Liang <953670473@qq.com>
2022-10-18 02:43:18 -0400

    adding discuss server and its api document

--- src/server/controllers/posts.js
+++ src/server/controllers/posts.js
@@ -1,4 +1,5 @@
-import Post from "../models/post.js"
+import Post from "../models/post.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
     try {
@@ -12,14 +13,39 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    const post = req.body;
-    const newPost = await Post.create(post);
-
+    const {title, message, creator, tags} = req.body;
+    const newPost = await Post.createPost({title, message, creator, tags});
     try {
         await newPost.save();
-
         res.status(201).json(newPost);
     } catch (error) {
-        
+        res.status(204).json({ message: error.message});
     }
+}
+
+export const getPostsByTags = async (req, res) => {
+    const tags = req.body.tags;
+    
+}
+
+export const getPost = async (req, res) => {
+    try {
+        const targetPost = await Post.findById(req.params.id);
+        res.status(200).json(targetPost);
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message});
+    }
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await Post.findById(id);
+
+    const updatedPost = await Post.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
 }
