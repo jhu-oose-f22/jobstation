b825d2cd8261f94d24ba43630b29db3145f6359b
qtniu <qitong.niu@gmail.com>
2022-10-19 14:57:45 -0400

    debug attempts

--- src/chatserver/index.js
+++ src/chatserver/index.js
@@ -29,41 +29,48 @@ connectDB();
 io.on('connect', (socket) => {
   // console.log(socket);
   socket.on('join', async ({ name, room }, callback) => {
-    console.log(name, room);
+    // console.log(name, room);
     // this should be add to the group database
+    // console.log(name, room);
+    console.log('-----------1-----------------------------------');
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
-
+    console.log('2');
+    console.log(user);
     socket.join(user.room);
+    console.log('3. joined');
     // fetch chat history from db and pass to frontend to display
-    //   response1.data.map((obj) => ({ text: obj.text, user: obj.user }))
-
     const msgs = await chatMessageController.fetchChatMessage(user.room);
+    console.log('fetched from db')
     const history = msgs.map((obj) => ({ text: obj.content, user: obj.user }));
-    console.log(history);
+    // console.log(history);
+    console.log('history mapped');
     socket.emit('history', history);
     // chatMessageController.createChatMessage(room,user.name);
 
-    socket.emit('message', { user: 'NightBot', text: `${user.name}, welcome to room ${user.room}.` });
-    socket.broadcast.to(user.room).emit('message', { user: 'NightBot', text: `${user.name} has joined!` });
+    // socket.emit('message', { user: 'NightBot', text: `${user.name}, welcome to room ${user.room}.` });
+    // socket.broadcast.to(user.room).emit('message', { user: 'NightBot', text: `${user.name} has joined!` });
 
-    console.log(`${user.name} joined the room ${room}`)
+    // console.log(`${user.name} joined the room ${room}`)
 
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
+    console.log('6');
     callback();
+    console.log('7');
+
   });
 
   socket.on('sendMessage', async (message, callback) => {
     const user = getUser(socket.id);
 
-    console.log(user.room);
+    // console.log(user.room);
+    console.log('sending------------')
     io.to(user.room).emit('message', { user: user.name, text: message });
 
     chatMessageController.storeChatMessage(user.room, message, user.name);
 
-    console.log(`a message: ${message} has been sent.`);
+    // console.log(`a message: ${message} has been sent.`);
 
     callback();
   });
