2d8781a7ac9cbaafd68ddb92f786d70207718658
Zhen Liang <953670473@qq.com>
2022-10-18 22:46:57 -0400

    Adding basic group function in server

---
+++ src/server/controllers/groups.js
@@ -0,0 +1,36 @@
+import Group from "../models/group.js";
+import mongoose from "mongoose";
+
+
+export const getGroup = async (req, res) => {
+    try {
+        const targetGroup = await Group.findById(req.params.id);
+        res.status(200).json(targetGroup);
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message});
+    }
+}
+
+export const createGroup = async (req, res) => {
+    const {groupName, owner, member, tags} = req.body;
+    const newGroup = await Group.createGroup({groupName, owner, member, tags});
+    try {
+        await newGroup.save();
+        res.status(201).json(newGroup);
+    } catch (error) {
+        res.status(204).json({ message: error.message});
+    }
+}
+
+export const addMember = async (req, res) => {
+    const { groupId, userId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(groupId)) return res.status(404).send(`No group with id: ${groupId}`);
+    //check whether member is valid after adding users
+    const group = await Group.findById(groupId);
+
+    group.member.push(userId);
+    await group.save();
+    res.json(group);
+}
--- src/server/controllers/posts.js
+++ src/server/controllers/posts.js
@@ -32,7 +32,6 @@ export const getPost = async (req, res) => {
     try {
         const targetPost = await Post.findById(req.params.id);
         res.status(200).json(targetPost);
-        
     } catch (error) {
         res.status(404).json({ message: error.message});
     }
@@ -40,12 +39,8 @@ export const getPost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
-
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    
     const post = await Post.findById(id);
-
     const updatedPost = await Post.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-    
     res.json(updatedPost);
 }
