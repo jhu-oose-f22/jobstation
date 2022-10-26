7e1e25c0fbf032c2ad35b632c49dd7f0f7f7264a
hehe-190 <18810660758@163.com>
2022-10-26 16:07:10 -0400

    Create tags.js

---
+++ src/server/models/tags.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const tagSchema = mongoose.Schema({
+    name: { 
+      type: String, 
+      required:  true 
+    },
+    id: { 
+      type: String 
+    },
+    createdAt: {
+      type: Date,
+      default: new Date()
+    },
+  });
+  
+  export default mongoose.model("User", tagSchema);
